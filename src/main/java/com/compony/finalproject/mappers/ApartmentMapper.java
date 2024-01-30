b3f8594c6d323eac0a06d7bb9a2de0f7ad3567c7
Serg <s.ustin.92@mail.ru>
2024-01-30 13:06:06 +0300

    add mappers

---
+++ src/main/java/com/compony/finalproject/mappers/ApartmentMapper.java
@@ -0,0 +1,12 @@
+package com.compony.finalproject.mappers;
+
+import com.compony.finalproject.dto.ApartmentDto;
+import com.compony.finalproject.model.Apartment;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ApartmentMapper {
+    ApartmentMapper INSTANCE = Mappers.getMapper(ApartmentMapper.class);
+    ApartmentDto toDto(Apartment apartment);
+}
---
+++ src/main/java/com/compony/finalproject/mappers/LandlordMapper.java
@@ -0,0 +1,12 @@
+package com.compony.finalproject.mappers;
+
+import com.compony.finalproject.dto.LandlordDto;
+import com.compony.finalproject.model.Landlord;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface LandlordMapper {
+    LandlordMapper INSTANCE = Mappers.getMapper(LandlordMapper.class);
+    LandlordDto toDto(Landlord landlord);
+}
