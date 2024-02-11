229a210980e3e212b972481c2062e86e002f5f7d
Serg <s.ustin.92@mail.ru>
2024-02-11 17:49:15 +0300

    code refactoring and test coverage

--- src/main/java/com/company/finalproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/company/finalproject/service/impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
-package com.compony.finalproject.service.impl;
+package com.company.finalproject.service.impl;
 
-import com.compony.finalproject.model.Accommodation;
-import com.compony.finalproject.model.User;
-import com.compony.finalproject.repository.UserRepository;
-import com.compony.finalproject.service.CRUDService;
-import jakarta.persistence.EntityNotFoundException;
+import com.company.finalproject.model.User;
+import com.company.finalproject.repository.UserRepository;
+import com.company.finalproject.service.CRUDService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,65 +11,55 @@ import java.util.NoSuchElementException;
 
 @Service
 public class UserServiceImpl implements CRUDService<User> {
-
     private final UserRepository userRepository;
-
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
     @Override
     public User getById(Long id) {
         return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
     }
-
     @Override
     public List<User> getAll() {
         return userRepository.findAll();
     }
-
     @Override
     public void create(User item) {
         // По умолчанию для нового пользователя статус в "ACTIVE"
         item.setStatus("ACTIVE");
-
+        item.setRating(0);
+        item.setNumberOfRatings(0L);
         // Сохранить пользователя в базу данных
         userRepository.save(item);
     }
-
-
+    @Override
+    public void edit(Long id, User item) {
+        userRepository.findById(id);
+        userRepository.save(item);
+    }
     public Long getUserIdByUsername(String username) {
         User user = userRepository.findByUsername(username);
         return user.getId();
     }
-
-    public void setRatingByFullName(String fullName, double newRating) {
+    public void setRatingByFullName(String fullName, int newRating) {
         User user = userRepository.findByFullName(fullName);
-
         if (user == null) {
             throw new IllegalArgumentException("User with full name " + fullName + " does not exist");
         }
-
-        user.incrementNumberOfRatings();
-
+        user.setNumberOfRatings(user.getNumberOfRatings() + 1);
         long countRating = user.getNumberOfRatings();
-
-        Double currentRatings = user.getRating();
-        if (currentRatings == null || currentRatings == 0.0) {
+        Integer currentRatings = user.getRating();
+        if (currentRatings == null || currentRatings == 0) {
             user.setRating(newRating);
         } else {
-            double newAverageRating = ((currentRatings * (countRating - 1)) + newRating) / countRating;
-
-            newAverageRating = Math.round(newAverageRating * 100.0) / 100.0;
-
-            user.setRating(newAverageRating);
+            long newAverageRating = (int) ((currentRatings * (countRating - 1)) + newRating) / countRating;
+            newAverageRating = (long) (Math.round(newAverageRating * 100.0) / 100.0);
+            user.setRating((int) newAverageRating);
             updateStatus(user);
         }
-
         userRepository.save(user);
     }
-
     private void updateStatus(User user) {
         // Если рейтинг пользователя больше или равен 2, то установить его статус в "ACTIVE"
         if (user.getRating() >= 2) {
@@ -81,33 +69,4 @@ public class UserServiceImpl implements CRUDService<User> {
             user.setStatus("BLOCKED");
         }
     }
-
-//    public void setRatingAndHandleBlock(String fullName, double rating) {
-//        // Найти пользователя по полному имени
-//        User user = userRepository.findByFullName(fullName);
-//
-//        // Установить рейтинг пользователя
-//        user.setRating(rating);
-//
-//        // Проверить, был ли пользователь ранее заблокирован
-//        if (user.getStatus().equals("BLOCKED")) {
-//            // Если пользователь был заблокирован, но его новый рейтинг больше 2, то разблокировать его
-//            if (rating >= 2) {
-//                user.setStatus("ACTIVE");
-//            }
-//        } else {
-//            // Если пользователь не был заблокирован, но его новый рейтинг меньше 2, то заблокировать его
-//            if (rating < 2) {
-//                user.setStatus("BLOCKED");
-//            }
-//        }
-//
-//        // Сохранить пользователя в базу данных
-//        userRepository.save(user);
-//    }
-
-
-
-
-
 }
--- src/test/java/com/company/finalproject/service/impl/UserServiceImplTest.java
+++ src/test/java/com/company/finalproject/service/impl/UserServiceImplTest.java
@@ -1,7 +1,7 @@
-package com.compony.finalproject.service.impl;
+package com.company.finalproject.service.impl;
 
-import com.compony.finalproject.model.User;
-import com.compony.finalproject.repository.UserRepository;
+import com.company.finalproject.model.User;
+import com.company.finalproject.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,8 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -62,6 +61,52 @@ class UserServiceImplTest {
 
         // Assert
         verify(repository).save(user); // Проверяем, что метод save был вызван с переданным пользователем
+    }
+
+    @Test
+    public void testEdit() {
+        // Создание фиктивного объекта User
+        User user = new User();
+
+        // Задание ожидаемого поведения при вызове метода findById
+        when(repository.findById(1L)).thenReturn(Optional.of(user));
+
+        // Вызов метода edit
+        service.edit(1L, user);
+
+        // Проверка, что метод findById был вызван один раз
+        verify(repository, times(1)).findById(1L);
+
+        // Проверка, что метод save был вызван один раз с аргументом user
+        verify(repository, times(1)).save(user);
+    }
+
+    @Test
+    public void testGetUserIdByUsername() {
+        // Создание фиктивного объекта User
+        User user = new User();
+        user.setId(123L);
+        when(repository.findByUsername("testUser")).thenReturn(user);
+
+        Long userId = service.getUserIdByUsername("testUser");
+        assertEquals(123L, userId);
+    }
+
+    @Test
+    public void testSetRatingByFullName() {
+        // Создание фиктивного объекта User
+        User user = new User();
+        user.setFullName("John Doe");
+        user.setNumberOfRatings(5L);
+        user.setRating(4);
+        when(repository.findByFullName("John Doe")).thenReturn(user);
+
+        service.setRatingByFullName("John Doe", 5);
+
+        // Проверка, что методы setRating и updateStatus были вызваны
+        assertEquals(6L, user.getNumberOfRatings());
+        assertEquals(4, user.getRating());
+        verify(repository, times(1)).save(user);
     }
 
 
